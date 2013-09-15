954fd110ed63f941d50a27d3c78ce38cf78a1b5c
Yury Stmol <yury@stmol.me>
2013-09-15 17:25:15 +0400

    Add screenshots and fancyBox

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,14 @@
 'use strict';
 
-(function(global, $) {
-
-})(window, jQuery);
+$(document).ready(function () {
+    $(".fancybox").fancybox({
+        padding: 2,
+        helpers: {
+            overlay: {
+                css: {
+                    'background': 'rgba(0, 0, 0, 0.2)'
+                }
+            }
+        }
+    });
+});
